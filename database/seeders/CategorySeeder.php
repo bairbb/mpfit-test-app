574ac52565bc27a68a2da16b377b107e85169cf8
bairbb <qiukbb@gmail.com>
2025-02-21 10:48:38 +0800

    init factory and seed Category and Product

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (['Легкий', 'Хрупкий', 'Тяжелый'] as $name) {
+            Category::create(['name' => $name]);
+        }
+    }
+}
